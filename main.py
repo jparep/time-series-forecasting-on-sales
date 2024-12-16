83a947701332d31acfe65810688fd9e6b1ba253a
jparep <parepson@gmail.com>
2024-12-16 11:29:43 -0700

    Import files into main file

--- main.py
+++ main.py
@@ -1,2 +1,5 @@
 from src.data_loader import laod_sales_data
-from forecasting import train_forecast_model, make_f
+from src.forecasting import train_forecast_model, make_forecast
+from src.evaluation import evaluate_forecast
+from src.visualization import plot_forecast
+
