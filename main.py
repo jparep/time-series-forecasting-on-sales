84747e990bb589e4aad4c7ca80546bcfea016c12
jparep <parepson@gmail.com>
2024-12-16 11:31:49 -0700

    Call load data method in the main

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-from src.data_loader import laod_sales_data
+from src.data_loader import load_sales_data
 from src.forecasting import train_forecast_model, make_forecast
 from src.evaluation import evaluate_forecast
 from src.visualization import plot_forecast
 
+# Load Data
+FILE_PATH = 'data/retail_sale.csv'
+df = load_sales_data(FILE_PATH)
+
+# Train
--- src/data_loader.py
+++ src/data_loader.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def laod_sales_data(file_path):
+def load_sales_data(file_path):
     """Load sale data from a CSV file"""
     data = pd.read_csv(file_path, parse_dates=["data"])
     data.sort_values(by='data', inplace=True)
