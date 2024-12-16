ebcebedd3ac17f745df9768bfbce48856b4ab47b
jparep <parepson@gmail.com>
2024-12-16 11:28:47 -0700

    Forecast evaluation method implemented

--- src/evaluation.py
+++ src/evaluation.py
@@ -1,13 +1,8 @@
-import matplotlib.pyplot as plt
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+import numpy as np
 
-def plot_forecast(data, forecast, target_col='sale'):
-    """Plot historical data with forecast."""
-    plt.figure(figsize=(10,6))
-    plt.plot(data['date'], data['target_col'], label='Acutal Sales')
-    plt.plot(forecast['ds'], forecast['yhat'], label='Forecast', linestyle='--')
-    plt.fill_between(forecast['ds'], forecast['yhat_lower'], forecast['yhat_upper'], alpha=0.2)
-    plt.xlabel('Date')
-    plt.ylabel('Sales')
-    plt.title('Retial Sales Forecast')
-    plt.legend()
-    plt.show()
+def evaluate_forecast(actual, predicted):
+    """Evaluate forecast using MAE, RMSE."""
+    mae = mean_absolute_error(actual, predicted)
+    rmse = np.sqrt(mean_squared_error(actual, predicted))
+    return {'MAE': mae, 'RMSE': rmse}
