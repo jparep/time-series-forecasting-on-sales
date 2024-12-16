3f07735fe30dfab9cc94787a971611eca7d5a549
jparep <parepson@gmail.com>
2024-12-16 10:57:19 -0700

    Load data method created

---
+++ src/data_loader.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def laod_sales_data(file_path):
+    """Load sale data from a CSV file"""
+    data = pd.read_csv(file_path, parse_dates=["data"])
+    data.sort_values(by='data', inplace=True)
+    return data
