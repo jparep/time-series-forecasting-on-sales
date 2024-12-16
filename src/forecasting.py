77adb96623c6ad63d2f320d04973a8ec28659ea6
jparep <parepson@gmail.com>
2024-12-16 11:21:35 -0700

    Make forecast method created

--- src/forecasting.py
+++ src/forecasting.py
@@ -6,3 +6,9 @@ def train_forecast_model(data, target_col='sale'):
     model = Prophet(yearly_seasonality=True, daily_seasonality=False, weekly_seasonality=False)
     model.fit(data)
     return model
+
+def make_forecast(model, periods=12, freq='M'):
+    "make future prediction using Prophet model"
+    future = model.make_future_dataframe(periods=periods, freq=freq)
+    forecast = model.predict(future)
+    return forecast
